e9cd5654fce956c430e4f1f59ba0560ab30c889a
sijawiir <raihanmarwanda@gmail.com>
2026-06-24 15:20:18 +0700

    fix: double scrollbar

--- src/app/(protected)/admin/layout.tsx
+++ src/app/(protected)/admin/layout.tsx
@@ -9,13 +9,13 @@ interface AdminLayoutProps {
 export default function AdminLayout({ children }: AdminLayoutProps) {
 
     return (
-        <div className="grow flex h-screen overflow-hidden bg-slate-100">
+        <div className="flex h-screen w-full min-h-0 overflow-hidden bg-slate-100">
             <AdminSidebar />
 
-            <div className="grow flex flex-col h-full overflow-hidden">
+            <div className="grow flex flex-col h-full min-h-0 overflow-hidden">
                 <AdminHeader />
 
-                <main className="grow pt-2 px-8 pb-6 overflow-y-auto">
+                <main className="grow min-h-0 pt-2 px-8 pb-6 overflow-y-auto">
                     {children}
                 </main>
             </div>
