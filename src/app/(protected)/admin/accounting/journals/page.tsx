39d52b2fb937f340af5367fd96c70539a85167c3
sijawiir <raihanmarwanda@gmail.com>
2026-07-21 10:03:34 +0700

    fix: balance sheet

--- src/app/(protected)/admin/accounting/journals/page.tsx
+++ src/app/(protected)/admin/accounting/journals/page.tsx
@@ -2,7 +2,7 @@ import { JournalsPage } from "@/features/accounting/components/journals/journals
 
 export default function AdminJournalsPage() {
     return (
-        <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
+        <div className="flex-1 space-y-4 pt-4">
             <JournalsPage />
         </div>
     );
